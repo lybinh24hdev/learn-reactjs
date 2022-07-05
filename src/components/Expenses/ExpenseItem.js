86d5e9afa92f5ba07dd585becfd13929dde831f2
lybinh24hdev <binhpham.24hdev@gmail.com>
2022-07-05 16:33:06 +0700

    getting started

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,13 @@
+import './ExpenseItem.css'
+
+
+function ExpenseItem ({title, amount}) {
+    return (
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${amount}</div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
