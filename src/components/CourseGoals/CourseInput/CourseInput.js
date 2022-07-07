a8efa43989bbf2665804b5a4b7060e1d8803d13c
lybinh24hdev <binhpham.24hdev@gmail.com>
2022-07-07 09:04:24 +0700

    styling react components - css module

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,44 @@
+import React, { useRef, useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const inputRef = useRef()
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+    setIsValid(true);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    } else {
+      props.onAddGoal(enteredValue);
+      setEnteredValue('');
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
+        <input
+          type="text"
+          ref={inputRef}
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
