a0a140fe27975d8229aeaa15e0904c24013fd35f
lybinh24hdev <binhpham.24hdev@gmail.com>
2022-07-14 11:40:14 +0700

    Working with form and users input

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
-import React, { useEffect, useState } from "react";
-
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
-import useHttp from "./hooks/useHttp";
+// import SimpleInput from './components/SimpleInput';
+import BasicForm from "./components/BasicForm";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const transformedData = (data) => {
-    const loadedTasks = [];
-    for (const taskKey in data) {
-      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp();
-
-  useEffect(() => {
-    fetchTasks(
-      {
-        url: "https://react-24hdev-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-      },
-      transformedData
-    );
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
+    <div className="app">
+      {/* <SimpleInput /> */}
+      <BasicForm />
+    </div>
   );
 }
 
