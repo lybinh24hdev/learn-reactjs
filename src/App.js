32d45cacc5e1dc55df1174cc78d8dcd031fd9c1b
lybinh24hdev <binhpham.24hdev@gmail.com>
2022-07-19 16:59:46 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cartItemActions";
-// import { fetchCartData } from "./store/cartItemActions"
-
-let isInitial = true;
-
 function App() {
-  const isShow = useSelector((state) => state.cart.isShow);
-  const notify = useSelector((state) => state.cart.notify);
-  const cartItem = useSelector((state) => state.cartItem.items);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // dispatch(fetchCartData(cartItem))
-
-    if (isInitial) {
-      isInitial = false;
-      return;
-    }
-    dispatch(sendCartData(cartItem));
-  }, [cartItem, dispatch]);
-
-  return (
-    <>
-      {notify && (
-        <Notification
-          status={notify.status}
-          title={notify.title}
-          message={notify.message}
-        />
-      )}
-      <Layout>
-        {isShow && <Cart />}
-        <Products />
-      </Layout>
-    </>
-  );
+  return <div>HELLO WORLD!</div>;
 }
 
 export default App;
