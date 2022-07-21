90ff8b1d5fc92249b0c36a0ab9fbf52f6ed2fcec
lybinh24hdev <binhpham.24hdev@gmail.com>
2022-07-21 11:53:22 +0700

    deploying react app: https://react-24hdev.web.app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
+import { lazy } from "react";
+import { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import AllQuotes from "./pages/AllQuotes";
-import AddQuote from "./pages/AddQuote";
-import QuoteDetail from "./pages/QuoteDetail";
+
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const AddQuote = lazy(() => import("./pages/AddQuote"))
+const QuoteDetail = lazy(() => import("./pages/QuoteDetail"));
+const NotFound = lazy(() => import("./pages/NotFound"))
+const AllQuotes = lazy(() => import("./pages/AllQuotes"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/add-quote">
-          <AddQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/add-quote">
+            <AddQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
